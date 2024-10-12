b0bf664ec44b5b838a986e7b181d10dc7089fb26
Daniel <dmarron@unsa.edu.pe>
2024-10-12 11:09:14 -0500

    segundo commit

---
+++ script.js
@@ -0,0 +1,66 @@
+document.querySelector('h1').textContent =  "¡Bienvenido a mi juego favorito!";
+
+const img1 = document.getElementById('img1');
+const description1 = document.getElementById('descripcion1');
+
+img1.addEventListener('click', () => {
+    if (description1.style.display === 'none') {
+        description1.style.display = 'block';
+
+    } else {
+        description1.style.display = 'none';
+    }
+
+});
+
+const img2 = document.getElementById('img2');
+const description2 = document.getElementById('descripcion2');
+
+img2.addEventListener('click', () => {
+    if (description2.style.display === 'none') {
+        description2.style.display = 'block';
+
+    } else {
+        description2.style.display = 'none';
+    }
+
+});
+
+const img3 = document.getElementById('img3');
+const description3 = document.getElementById('descripcion3');
+
+img3.addEventListener('click', () => {
+    if (description3.style.display === 'none') {
+        description3.style.display = 'block';
+
+    } else {
+        description3.style.display = 'none';
+    }
+
+});
+
+const img4 = document.getElementById('img4');
+const description4 = document.getElementById('descripcion4');
+
+img4.addEventListener('click', () => {
+    if (description4.style.display === 'none') {
+        description4.style.display = 'block';
+
+    } else {
+        description4.style.display = 'none';
+    }
+
+});
+
+const img5 = document.getElementById('img5');
+const description5 = document.getElementById('descripcion5');
+
+img5.addEventListener('click', () => {
+    if (description5.style.display === 'none') {
+        description5.style.display = 'block';
+
+    } else {
+        description5.style.display = 'none';
+    }
+
+});
